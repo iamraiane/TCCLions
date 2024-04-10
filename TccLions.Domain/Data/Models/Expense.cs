6a454d270140f21a38de1418fc8f1d0dc1982201
Raiane Cecílio <cecilioraiane371@gmail.com>
2024-04-10 11:59:47 -0300

    Created Project

---
+++ TccLions.Domain/Data/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TccLions.Domain.Data.Models
+{
+    public class Expense
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public DateOnly ExpirationDate { get; set; }
+        public DateOnly RegistrationDate { get; set; }
+        public double Value { get; set; }
+    }
+}
