8c7af2fd362638ee40a57539d939844e0d6fb742
Cauã Belodi Cardoso <cauacardosoyt@gmail.com>
2024-05-25 10:40:33 -0300

    Transloco and Language switch added

--- TccLions.Web/src/app/app.config.ts
+++ TccLions.Web/src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideTransloco({
+        config: {
+          availableLangs: ['en', 'pt'],
+          defaultLang: 'pt',
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })]
 };
--- TccLions.Web/src/app/modules/shared/navbar/navbar.component.ts
+++ TccLions.Web/src/app/modules/shared/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import { TRANSLOCO_SCOPE, TranslocoModule, TranslocoService } from '@jsverse/transloco';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatSelectModule, TranslocoModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  private _translocoService = inject(TranslocoService)
 
+  translate() {
+    this._translocoService.setActiveLang(this._translocoService.getActiveLang() === 'en' ? 'pt' : 'en');
+  }
 }
